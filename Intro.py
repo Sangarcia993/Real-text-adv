24c40e6677f1d2ffa28c8d51bdf750459d73581b
Sangarcia993 <66141661+Sangarcia993@users.noreply.github.com>
2021-01-26 23:08:01 -0300

    Added city.py and intro.py

---
+++ Intro.py
@@ -0,0 +1,15 @@
+import time
+
+print("Welcum to the city of Night")
+
+#creating suspense
+i = 1
+while i < 4:
+    print(".")
+    time.sleep(1)
+    i += 1
+
+print("Great adventurer, what is your name?")
+name = input(">>> ")
+
+print("Great, it is an honor to wlcome you,", name, "to the city.\nI recomend you go to the caves to explore")
