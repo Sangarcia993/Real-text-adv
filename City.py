a4836d18813b3331ce5cc733a94ad3d0641cc4ce
Sangarcia993 <66141661+Sangarcia993@users.noreply.github.com>
2021-02-01 15:01:52 -0300

    adding rps

--- City.py
+++ City.py
@@ -4,8 +4,17 @@ class Casino():
         self.wallet = p_player_coins
         self.card = 0
         self.debt = 0
-    
-    def RPS(sefl):
-        pass
 
-    
+    #Rock-paper-scicors game
+    def RPS(self):
+        bet = input("How much money do you want to bet?")
+        if bet == "0":
+            print("Next time actually bet something")
+        elif int(bet) > self.wallet:
+            print("You cant bet more than what you have:", self.wallet)
+        
+
+
+Casino1 = Casino(100)
+
+Casino1.RPS()
